e338d486f71e3659293a8322504606484cbdd6d4
faiyyazs <faiyyazshaikh2010@gmail.com>
2022-09-15 12:48:48 +0530

    Selector Presenter impl

--- flutter-errors/lib/src/flutter/presenter/selector_error_presenter.dart
+++ flutter-errors/lib/src/flutter/presenter/selector_error_presenter.dart
@@ -1,7 +1,12 @@
 import '../../../flutter_errors.dart';
 
-abstract class SelectorErrorPresenter<T> implements FlutterErrorPresenter<T> {
+class SelectorErrorPresenter<T> implements FlutterErrorPresenter<T> {
   final FlutterErrorPresenter<T> Function(Exception) errorPresenterSelector;
 
   SelectorErrorPresenter(this.errorPresenterSelector);
+
+  @override
+  void show(Exception throwable, FlutterWidgetBindingObserver activity, T data) {
+    errorPresenterSelector(throwable).show(throwable, activity, data);
+  }
 }
