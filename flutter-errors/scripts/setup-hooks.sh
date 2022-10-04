45f0d96f11538c1141df3295647a1800bd45ec77
faiyyazs <faiyyazshaikh2010@gmail.com>
2022-10-04 23:55:25 +0530

    Removed release drafter & added manual release

--- flutter-errors/scripts/setup-hooks.sh
+++ flutter-errors/scripts/setup-hooks.sh
@@ -2,10 +2,10 @@
 
 echo Marking all hooks as executable...
 
-find scripts/hooks/ -type f -exec chmod +x {} \;
+find flutter-errors/scripts/hooks/ -type f -exec chmod +x {} \;
 
 echo All hooks marked as executable
 
 echo Moving all hooks to .git/hooks/
 
-rsync -P scripts/hooks/* .git/hooks/
+rsync -P flutter-errors/scripts/hooks/* .git/hooks/
